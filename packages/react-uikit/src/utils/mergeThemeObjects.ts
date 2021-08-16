ad2a52b04045a4a2f2c00fc208e0d0412e9318ad
Alexander Rudenko <learnitnow@yandex.ru>
2021-08-16 20:19:30 +0300

    fix(FJS-NOINDEX): enabling rules for eslint

--- packages/react-uikit/src/utils/mergeThemeObjects.ts
+++ packages/react-uikit/src/utils/mergeThemeObjects.ts
@@ -1,24 +1,26 @@
-import utils from '../../../utils'
+import { Obj } from '@fast-js/utils';
 
 /**
  * merge main and custom theme objects
- * @param mainTheme 
- * @param customStyles 
+ * @param {any} mainTheme
+ * @param {any} customStyles
+ * @returns {any}
  */
-export const mergeThemeObjects = (mainTheme: Object, customStyles: Object): any => {
+export const mergeThemeObjects = (mainTheme: any, customStyles: any): any => {
     const theme = { ...mainTheme };
     const custom = { ...customStyles };
     let result = {};
 
-    if (!!mainTheme) {
-        if (!!customStyles) {
-            result = utils.mergeDeep(theme, custom);
+    if (mainTheme) {
+        if (customStyles) {
+            result = Obj.mergeDeep(theme, custom);
         } else {
             result = {
                 ...theme,
-            }
+            };
         }
     }
+
     return result;
 };
 
