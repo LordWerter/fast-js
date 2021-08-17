52f4d916a52237292de4875db92c2a55d824c1b7
Alexander Rudenko <learnitnow@yandex.ru>
2021-08-17 17:40:02 +0300

    fix(FJS-NOINDEX): react-uikit core corrections

--- packages/react-uikit/src/components/Table/components/Row/Row.tsx
+++ packages/react-uikit/src/components/Table/components/Row/Row.tsx
@@ -10,14 +10,27 @@ export interface IProps {
 
 import { CWrap, Ceil } from './Row.styles';
 
-export const Row: React.FC<IProps> = ({ sizeId, ceils }) => {
+export const Row: React.FC<IProps> = (props) => {
+    const { sizeId = 'm', ceils } = props;
+
     return (
         <CWrap sizeId={sizeId}>
-            {ceils.map(({ width, value }, index: number) => (
-                <Ceil key={index} width={width} sizeId={sizeId}>
-                    {value}
-                </Ceil>
-            ))}
+            {ceils.map(
+                (
+                    ceil: {
+                        width: string;
+                        value: string;
+                    },
+                    index: number
+                ) => {
+                    const { width, value } = ceil;
+                    return (
+                        <Ceil key={index} width={width} sizeId={sizeId}>
+                            {value}
+                        </Ceil>
+                    );
+                }
+            )}
         </CWrap>
     );
 };
