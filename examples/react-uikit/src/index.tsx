36e1afc3d5b3afe0fddbfb3230cedb3a289c0616
Evgeniy <46823312+Evgenie@users.noreply.github.com>
2021-09-07 17:08:17 +0300

    feat(FJS-3): the specified folder and file structure is created

--- examples/react-uikit/src/index.tsx
+++ examples/react-uikit/src/index.tsx
@@ -1,1 +1,21 @@
-export {}
+import React from "react";
+import { useTheme } from "@emotion/react";
+import {CWrap} from '.ImageSlides.styles';
+
+export interface IProps {
+    sizeId?: string;
+    langId?: string;
+    customise?: unknown;
+}
+
+export const ImageSlides: React.FC = (props): JSX.Element => {
+    const {sizeId = 'mobile', langId = 'ru', customise = null} = props;
+    console.log(sizeId, langId, customise);
+    return (
+        <CWrap>
+            {`Hello`}
+        </CWrap>
+    );
+};
+
+export default ImageSlides;
