08bc92f6b4446ead5d6f5db41450e125986f0338
Evgeniy <46823312+Evgenie@users.noreply.github.com>
2021-11-14 18:42:45 +0300

    fix(FJS-3): renamed components

--- examples/react-uikit/src/features/NavBar/NavBar.tsx
+++ examples/react-uikit/src/features/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { useTheme } from '@emotion/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { TElementProps } from '../../definitions';
-import { CWrap, SeparatorMiddle } from './NavBar.styles';
+import { CWrap, Line } from './NavBar.styles';
 
 export type IProps = TElementProps & {
     visibility: boolean;
@@ -16,7 +16,7 @@ const NavBar: React.FC<IProps> = (props): JSX.Element => {
     return (
         <CWrap sizeId={'desktop'} theme={visibility ? theme.cwrap : theme.hidden }>
             <Link to={'/'}>Главная</Link>
-            <SeparatorMiddle sizeId={'desktop'} theme={theme.separatorMiddle} />
+            <Line sizeId={'desktop'} theme={theme.line} />
             <Link to={'/contacts'}>Контакты</Link>
         </CWrap>
     )
